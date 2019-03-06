f8a853124f77d22dd08bbe91f061e95c09f4b6af
biyinlong <biyinlong@thinkit.cn>
2019-03-06 17:31:51 +0800

    ParamASRStep.java 设置默认月份

--- src/main/java/entities/step/ParamASRStep.java
+++ src/main/java/entities/step/ParamASRStep.java
@@ -6,6 +6,8 @@ import entities.action.Action;
 import exception.NoMatchConditionException;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -23,6 +25,33 @@ public class ParamASRStep extends ASRStep{
     public void run(StateMachine stateMachine) throws Exception{
         try {
             onEnter(stateMachine);
+
+            //如果是以下几个流程，当用户未说出月份时，默认为当月进行处理
+            HashSet<String> businesSet = new HashSet<String>() {{
+                add("nx005_查询账单");
+                add("nx120_核对话费去向");
+                add("nx138_核对通讯费产生");
+                add("nx139_核对流量额外收费");
+                add("nx140_核对增值业务费");
+                add("nx141_核对其他费用");
+            }};
+            String processName = stateMachine.getCurrentProcess().getName();
+            if (businesSet.contains(processName) && bindParam.equals("month") &&
+                    stateMachine.getStatementParamMap().get(bindParam).getValue().isEmpty()){
+                Calendar cale = Calendar.getInstance();
+                int year = cale.get(Calendar.YEAR);
+                int currentMonth = cale.get(Calendar.MONTH) + 1;
+                String month = "";
+                if(currentMonth >= 1 && currentMonth <= 9){
+                    month = String.valueOf(year) + "0" + String.valueOf(currentMonth);
+                    stateMachine.getStatementParamMap().get(bindParam).setValue(String.valueOf(month));
+                }
+                if(currentMonth >= 10){
+                    month = String.valueOf(year) + String.valueOf(currentMonth);
+                    stateMachine.getStatementParamMap().get(bindParam).setValue(String.valueOf(month));
+                }
+            }//如果是以上业务设置默认参数month
+
             if(stateMachine.getStatementParamMap().get(bindParam).getValue().isEmpty()){
                 interrupt(stateMachine);
                 run(stateMachine);
